501b9e355e89d9916fc282d34a13ecdbc3895d65
rush84 <rushabhs@andrew.cmu.edu>
2017-12-12 09:46:09 -0500

    Final commit

---
+++ SortedLinkedList.java
@@ -0,0 +1,62 @@
+/**
+ * @author Rushabh Shah
+ * Andrew id: rushabhs
+ *
+ */
+public class SortedLinkedList implements MyListInterface {
+    /**
+     * A reference to the head of the LinkedList
+     */
+    private Node head;
+    
+    public SortedLinkedList() {
+        
+    }
+    
+    public SortedLinkedList(String[] unsorted) {
+        
+    }
+    
+    @Override
+    public void add(String value) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int size() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void display() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean contains(String key) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String removeFirst() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String removeAt(int index) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
